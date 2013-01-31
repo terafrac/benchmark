df2e2ced926750741d49dc19fa68f25d86379b61
David Bernstein <david.bernstein@meliortechnology.com>
2013-01-30 17:29:25 -0800

    Fixed boundary conditions and changed to second order elements.  Tip deflection is very close to slender beam model.

--- OpenFracCantileverBenchmark1.py
+++ OpenFracCantileverBenchmark1.py
@@ -19,7 +19,7 @@ set_log_level(DEBUG)
 # material properties
 rho = 2200.0
 youngs_modulus = 15e9
-poisson_ratio = 0.0
+poisson_ratio = 0.1
 
 mu = youngs_modulus / (2.0 * (1+poisson_ratio))
 lmbda = youngs_modulus * poisson_ratio / ((1 + poisson_ratio)*(1 - 2 * poisson_ratio))
@@ -33,26 +33,25 @@ top_pressure = 100.0
 box_x = beam_length
 box_y = beam_width
 box_z = beam_width
-element_size = 0.3
+element_size = 0.5
 
 num_elements_x = int(box_x / element_size)
 num_elements_y = int(box_y / element_size)
 num_elements_z = int(box_z / element_size)
 
-mesh = BoxMesh(0, 0, -box_z, box_x, box_y, 0.0, \
-               num_elements_x, num_elements_y, num_elements_z)
+mesh = BoxMesh(0, 0, -box_z, box_x, box_y, 0.0, num_elements_x, num_elements_y, num_elements_z)
 
-boundary_parts = MeshFunction("uint", mesh, mesh.topology().dim()-1)
+boundary_parts = MeshFunction("size_t", mesh, mesh.topology().dim()-1)
 
 #plot(mesh, interactive=True)
 
 # Dirichlet condition on right boundary
 def boundary(x, on_boundary):
-    return on_boundary and x[0] < DOLFIN_EPS
+    return on_boundary and x[0] < 1e-3;
 
 class TopBoundary(SubDomain):
     def inside(self, x, on_boundary):
-        return on_boundary and x[2] < DOLFIN_EPS;
+        return on_boundary and x[2] > -1e-3;
 
 Gamma_Top = TopBoundary()
 Gamma_Top.mark(boundary_parts, 0)
@@ -62,7 +61,7 @@ top_face_load = Constant((0,0,top_pressure))
 
 
 # Define function space
-V = VectorFunctionSpace(mesh, "Lagrange", 1)
+V = VectorFunctionSpace(mesh, "Lagrange", 2)
 
 # Test and trial functions
 u = TrialFunction(V)
@@ -80,7 +79,7 @@ def sigma(v):
     return 2*mu*epsilon(v) + lmbda*tr(epsilon(v))*Identity(v.cell().d)
 
 # balance equation
-F = inner(sigma(u), grad(v))*dx + inner(v,top_face_load)*ds
+F = inner(sigma(u), grad(v))*dx + inner(v,top_face_load)*ds(0)
 
 # Extract bilinear and linear forms from F
 a = lhs(F)
@@ -104,6 +103,8 @@ tip_deflection = 0.0
 coordinates = mesh.coordinates()
 #plot(u_z, interactive=True)
 num_pts = 0
+parameters["allow_extrapolation"] = True
+
 for i in range(mesh.num_vertices()):
     if abs(coordinates[i][0] - beam_length) < DOLFIN_EPS:
         tip_deflection += u(coordinates[i])[2]
@@ -117,7 +118,7 @@ print tip_deflection_model
 relative_error = abs(tip_deflection / tip_deflection_model - 1.0)
 
 print "Tip deflection is " + str(tip_deflection)
-if relative_error < 1e-3:
+if relative_error < 5e-4:
     print "OpenFrac benchmark passed."
     sys.exit(0)
 else:
